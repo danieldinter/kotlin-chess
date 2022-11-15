0e3d40aa6fb855728d31df8cc21a84720aee1dd3
danieldinter <danieldinter@users.noreply.github.com>
2022-11-15 16:20:08 +0100

    Update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.21"
     application
     id("org.openjfx.javafxplugin") version "0.0.13"
 }
